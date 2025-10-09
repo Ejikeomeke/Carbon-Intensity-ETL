a3f3c5226421f6f15cb941eef63072c80707e111
Ejikeomeke <engromeke@gmail.com>
2025-10-09 07:26:35 +0100

    entry point script added

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+if [ -e "/opt/airflow/requirements.txt" ]; then
+  $(command python) pip install --upgrade pip
+  $(command -v pip) install --user -r requirements.txt
+fi
+
+
+$(command -v airflow) db upgrade
+
+exec airflow webserver
